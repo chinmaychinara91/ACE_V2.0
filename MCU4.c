d803d31de5f47780c22a53e6333eb9e564b7e8d5
Chinmay Chinara <chinmaychinara91@users.noreply.github.com>
2016-08-28 15:51:51 -0700

    Add files via upload
    These have the embedded micro-controller codes that controlled all the servo motors used across the humanoid.

---
+++ MCU4.c
@@ -0,0 +1,126 @@
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <lcd.h>
+
+void USARTInit(uint16_t);
+char USARTReadChar(void);
+void USARTWriteChar(char);
+void Init_PWM(void);
+
+void USARTInit(uint16_t ubrr_value)
+{
+ UBRRL = ubrr_value;
+ UBRRH = (ubrr_value>>8);
+ UCSRC=(1<<URSEL)|(3<<UCSZ0);
+ UCSRB=(1<<RXEN)|(1<<TXEN);
+}
+
+char USARTReadChar(void)
+{
+ while(!(UCSRA & (1<<RXC)));
+ return UDR;
+}
+
+void USARTWriteChar(char data)
+{
+ while(!(UCSRA & (1<<UDRE)));
+ UDR=data;
+}
+
+void Init_PWM(void)
+{
+ TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        
+ TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); 
+ ICR1=2499; 
+
+ DDRD|=(1<<PD4)|(1<<PD5);
+}
+
+int main(void)
+{
+ char data;
+ char add1,add2;
+ char vl,vh,value;
+ TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        
+ TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); 
+ ICR1=2499; 
+ value = 0x00;
+
+ InitLCD(0);
+ Init_PWM();
+
+ USARTInit(51);
+ LCDClear();
+ LCDWriteStringXY(0,0,"Initialising...");
+ _delay_ms(1000);
+ 
+ LCDClear();
+ LCDWriteStringXY(0,0,"RFOT=");
+ LCDWriteStringXY(0,1,"RKNE=");
+
+ while(1)
+ {
+  data = USARTReadChar();
+
+  _delay_ms(100);
+  add1 = data & 0xf0;
+  switch (add1)
+  {
+   case 0x40:  //64
+   vl = data & 0x0f;
+   data = USARTReadChar();
+   add2 = data & 0xf0;
+   if (add2 == 0xC0) //192
+	{
+	 vh = data<<4;
+	 value =  vl | vh;
+	 if ((value >= 0) && (value <= 500))
+	 {
+	  OCR1A = 90+(10+value); //rfot
+	  LCDWriteIntXY(6,0,value,3);
+	  _delay_ms(1000);
+     }
+	 else
+	 { 
+	 	LCDWriteStringXY(6,0,"inv angle");
+		_delay_ms(1000);
+	 }
+	 break;
+    }
+   else 
+   break;
+   break;
+  
+   case 0x50: //80
+   vl = data & 0x0f;
+   data = USARTReadChar();
+   add2 = data & 0xf0;
+   if (add2 == 0xD0) //208
+	{
+	 vh = data<<4;
+	 value =  vl | vh;
+	 if ((value >= 0) && (value <= 500))
+     {
+      OCR1B = 90+(10+value); //rkne
+	  LCDWriteIntXY(6,1,value,3);
+	  _delay_ms(1000);
+     }
+	 else
+	 {
+	 	LCDWriteStringXY(6,1,"inv angle");
+		_delay_ms(1000);
+	 }
+	 break;
+    }
+   else 
+   break;
+   break;
+   default:
+   add1 = 0xff;
+  }
+ }
+ return 1;
+}
---
+++ MCU5.c
@@ -0,0 +1,122 @@
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <lcd.h>
+
+void USARTInit(uint16_t);
+char USARTReadChar(void);
+void USARTWriteChar(char);
+void Init_PWM(void);
+
+void USARTInit(uint16_t ubrr_value)
+{
+ UBRRL = ubrr_value;
+ UBRRH = (ubrr_value>>8);
+ UCSRC=(1<<URSEL)|(3<<UCSZ0);
+ UCSRB=(1<<RXEN)|(1<<TXEN);
+}
+
+char USARTReadChar(void)
+{
+ while(!(UCSRA & (1<<RXC)));
+ return UDR;
+}
+
+void USARTWriteChar(char data)
+{
+ while(!(UCSRA & (1<<UDRE)));
+ UDR=data;
+}
+
+void Init_PWM(void)
+{
+ TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        
+ TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); 
+ ICR1=2499; 
+
+ DDRD|=(1<<PD4)|(1<<PD5);
+}
+
+int main(void)
+{
+ char data;
+ char add1,add2;
+ char vl,vh,value;
+ value = 0x00;
+
+ InitLCD(0);
+ Init_PWM();
+
+ USARTInit(51);
+ LCDClear();
+ LCDWriteStringXY(0,0,"Initialising...");
+ _delay_ms(1000);
+ 
+ LCDClear();
+ LCDWriteStringXY(0,0,"RTHG=");
+ LCDWriteStringXY(0,1,"RHIP=");
+ while(1)
+ {
+  data = USARTReadChar();
+
+  _delay_ms(100);
+  add1 = data & 0xf0;
+  switch (add1)
+  {
+   case 0x60:  //96
+   vl = data & 0x0f;
+   data = USARTReadChar();
+   add2 = data & 0xf0;
+   if (add2 == 0xE0) //224
+	{
+	 vh = data<<4;
+	 value =  vl | vh;
+	 if ((value >= 0) && (value <= 500))
+	 {
+	  OCR1A = 90+(10+value); //rthg
+	  LCDWriteIntXY(6,0,value,3);
+	  _delay_ms(1000);
+     }
+	 else
+	 { 
+	 	LCDWriteStringXY(6,0,"inv angle");
+		_delay_ms(1000);
+	 }
+	 break;
+    }
+   else 
+   break;
+   break;
+  
+   case 0x70: //112
+   vl = data & 0x0f;
+   data = USARTReadChar();
+   add2 = data & 0xf0;
+   if (add2 == 0xF0) //240
+	{
+	 vh = data<<4;
+	 value =  vl | vh;
+	 if ((value >= 0) && (value <= 500))
+     {
+      OCR1B = 90+(10+value); //rhip
+	  LCDWriteIntXY(6,1,value,3);
+	  _delay_ms(1000);
+     }
+	 else
+	 {
+	 	LCDWriteStringXY(6,1,"inv angle");
+		_delay_ms(1000);
+	 }
+	 break;
+    }
+   else 
+   break;
+   break;
+   default:
+   add1 = 0xff;
+  }
+ }
+ return 1;
+}
